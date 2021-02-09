95007424059b98d321d7e1fb6cccbfff4d51f7dd
izzybogart <60237226+izzybogart@users.noreply.github.com>
2021-02-09 09:41:50 -0700

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -18,10 +18,10 @@ int main(){
     matrix a_stat, a_dy, b_stat, b_dy;
 
     static T data[] = {2,1,8,2,4,5,7,2,10};
-    a_stat = create_initvals_stat(2,5,data);
+    a_stat = create_initvals(2,5,data);
     a_dy = create_initvals(2,5,data);
 
-    b_stat = create_empty_stat(5,2);
+    b_stat = create_empty(5,2);
     b_dy = create_empty(5,2);
 
     transpose(a_stat, b_stat);
